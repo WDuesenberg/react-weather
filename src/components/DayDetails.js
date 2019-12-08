6c18aeddfb761aadc316747a1a6227ae587e31a7
w_duse89 <wyatt.duesenberg@gmail.com>
2019-12-07 19:52:13 -0700

    updates

--- src/components/DayDetails.js
+++ src/components/DayDetails.js
@@ -3,13 +3,13 @@ import { Col, Card, CardHeader, CardBody } from 'reactstrap';
 import styled from 'styled-components';
 
 const border = "2px solid green"
-const DayWrapper = styled.article `
+const DayWrapper = styled.article`
         .card {
             border: ${props => props.isActive ? "2px solid teal" : "1px solid blue"};
             text-align: center; 
         }
         img {
-            width: 55px;
+            width: 175px;
             border: ${border}
         }
     }
@@ -24,15 +24,17 @@ const DayDetails = props => {
                 <DayWrapper isActive={props.isActive}>
                     <Card>
                         <CardHeader><h2>Detailed Weather Info for {props.day}</h2></CardHeader>
-                        <img src={`${process.env.PUBLIC_URL}/icons/${props.icon}.png`} alt={props.description} />
-                        <p><strong>High:</strong> {props.high}</p>
-                        <p><strong>Low:</strong> {props.low}</p>
-                        <p><strong>UV Index:</strong> {props.uvIndex}</p>
-                        <p><strong>Wind Speed:</strong> {props.windSpeed}</p>
-                        <p><strong>Wind Gust Speed:</strong> {props.windGust}</p>
-                        <p><strong>Wind Direction:</strong> {props.windDir}</p>
-                        <p><strong>Probability of Precipitation:</strong> {props.probPrecip}</p>
-                        <p><strong>Humidity:</strong> {props.humidity}</p>
+                        <CardBody>
+                            <img src={`${process.env.PUBLIC_URL}/icons/${props.icon}.png`} alt={props.description} />
+                            <p><strong>High:</strong> {props.high}</p>
+                            <p><strong>Low:</strong> {props.low}</p>
+                            <p><strong>UV Index:</strong> {props.uvIndex}</p>
+                            <p><strong>Wind Speed:</strong> {props.windSpeed}</p>
+                            <p><strong>Wind Gust Speed:</strong> {props.windGust}</p>
+                            <p><strong>Wind Direction:</strong> {props.windDir}</p>
+                            <p><strong>Probability of Precipitation:</strong> {props.probPrecip}</p>
+                            <p><strong>Humidity:</strong> {props.humidity}</p>
+                        </CardBody>
                     </Card>
                 </DayWrapper>
             </Col>
