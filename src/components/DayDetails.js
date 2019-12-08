450795cf378d7e1b8b7dcffdd2d03dd5ca0b1d55
w_duse89 <wyatt.duesenberg@gmail.com>
2019-12-07 19:42:40 -0700

    updates

--- src/components/DayDetails.js
+++ src/components/DayDetails.js
@@ -23,16 +23,16 @@ const DayDetails = props => {
             <Col onClick={props.setSelectedDay}>
                 <DayWrapper isActive={props.isActive}>
                     <Card>
-                    <h2>Detailed Weather Info for {props.day}:</h2>
-                    <img src={`${process.env.PUBLIC_URL}/icons/${props.icon}.png`} alt={props.description} />
-                    <p><strong>High:</strong> {props.high}</p>
-                    <p><strong>Low:</strong> {props.low}</p>
-                    <p><strong>UV Index:</strong> {props.uvIndex}</p>
-                    <p><strong>Wind Speed:</strong> {props.windSpeed}</p>
-                    <p><strong>Wind Gust Speed:</strong> {props.windGust}</p>
-                    <p><strong>Wind Direction:</strong> {props.windDir}</p>
-                    <p><strong>Probability of Precipitation:</strong> {props.probPrecip}</p>
-                    <p><strong>Humidity:</strong> {props.humidity}</p>
+                        <CardHeader><h2>Detailed Weather Info for {props.day}</h2></CardHeader>
+                        <img src={`${process.env.PUBLIC_URL}/icons/${props.icon}.png`} alt={props.description} />
+                        <p><strong>High:</strong> {props.high}</p>
+                        <p><strong>Low:</strong> {props.low}</p>
+                        <p><strong>UV Index:</strong> {props.uvIndex}</p>
+                        <p><strong>Wind Speed:</strong> {props.windSpeed}</p>
+                        <p><strong>Wind Gust Speed:</strong> {props.windGust}</p>
+                        <p><strong>Wind Direction:</strong> {props.windDir}</p>
+                        <p><strong>Probability of Precipitation:</strong> {props.probPrecip}</p>
+                        <p><strong>Humidity:</strong> {props.humidity}</p>
                     </Card>
                 </DayWrapper>
             </Col>
